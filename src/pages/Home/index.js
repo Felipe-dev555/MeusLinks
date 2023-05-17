1c65734547717c103c97f91c8f2aeafb71bd8af3
Felipe-dev555 <felipesingilllo.e@gmail.com>
2023-05-17 17:47:51 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,28 @@
+import { FiLink } from 'react-icons/fi'
+import './home.css';
+
+export default function Home() {
+    return (
+        <div className="container-home">
+
+              
+            <div className="logo">
+                <img src="link.svg" alt="Sujeito Link Logo" />
+                <h1> Sujeito Link</h1>
+                <span>Cole seu link para encurtar</span>
+            </div>
+
+            <div className="area-input">
+                <div>
+                    <FiLink size={24} color='#FFF' />
+                    <input
+                        placeholder='Cole seu link aqui...'
+                    />
+                </div>
+
+                <button>Encurtar Link</button>
+
+            </div>
+        </div>
+    )
+}
